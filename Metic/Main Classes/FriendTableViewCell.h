eab1f8766e7e1ab75c8c2407b3b4c7530a306f7a
wjling <wjlingOo@126.com>
2014-05-30 17:33:29 +0800

    5.29 1. 好友中心增加了好友列表的粗略显示

---
+++ Metic/Main Classes/FriendTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  FriendTableViewCell.h
+//  Metic
+//
+//  Created by ligang5 on 14-5-30.
+//  Copyright (c) 2014年 dishcool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *title;
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+
+@end
--- Metic/Main Classes/FriendsViewController.h
+++ Metic/Main Classes/FriendsViewController.h
@@ -8,7 +8,23 @@
 
 #import <UIKit/UIKit.h>
 #import "SlideNavigationController.h"
+#import "HttpSender.h"
+#import "CommonUtils.h"
+#import "SlideNavigationController.h"
+#import "AppConstants.h"
+#import "MTUser.h"
+//#import "MTEvent.h"
+#import "AppDelegate.h"
+//#import "MTEvent.h"
+#import "MJRefreshHeaderView.h"
+#import "FriendTableViewCell.h"
 
-@interface FriendsViewController : UITableViewController <SlideNavigationControllerDelegate>
+@interface FriendsViewController : UIViewController <SlideNavigationControllerDelegate,HttpSenderDelegate,UITableViewDelegate,UITableViewDataSource,MJRefreshBaseViewDelegate>
 
+@property(nonatomic,strong)MTUser *user;
+@property(nonatomic,strong)NSMutableArray* friendList;
+@property (strong, nonatomic) IBOutlet UITableView *friendTableView;
+
+
+- (void)synchronize_friends;
 @end
