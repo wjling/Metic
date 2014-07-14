7d8bb31107e844038ac6d7aae633af898a6b2ad3
dishcool <ligang_mac4@ligang-mac4deiMac.local>
2014-07-14 18:49:31 +0800

    7.14 加入百度地图sdk，调试中

---
+++ Metic/Main Classes/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Metic
+//
+//  Created by ligang_mac4 on 14-7-14.
+//  Copyright (c) 2014年 dishcool. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MapViewController : ViewController
+
+@end
