84e8c775ebf7b3311f1471c0eeb67800352d0012
dishcool <ligang_mac4@ligang-mac4deiMac.local>
2014-07-28 17:30:36 +0800

    7.28.3 活动邀请 开发准备

---
+++ Metic/Cell/EventInvitationCell.h
@@ -0,0 +1,13 @@
+//
+//  EventInvitationCell.h
+//  Metic
+//
+//  Created by ligang_mac4 on 14-7-28.
+//  Copyright (c) 2014年 dishcool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventInvitationCell : UITableViewCell
+
+@end
---
+++ Metic/Main Classes/EventInvitationViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventInvitationViewController.h
+//  Metic
+//
+//  Created by ligang_mac4 on 14-7-28.
+//  Copyright (c) 2014年 dishcool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventInvitationViewController : UIViewController
+
+@end
