757ea13ffcd05a49321a12b16745fcb3ad82ca54
JJ <185597569@qq.com>
2014-09-23 14:09:15 +0800

    9.23.1 按照赞排序 初始版本

---
+++ Metic/Cell/PhotoRankingTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PhotoRankingTableViewCell.h
+//  WeShare
+//
+//  Created by ligang6 on 14-9-23.
+//  Copyright (c) 2014年 dishcool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoRankingTableViewCell : UITableViewCell
+
+@end
---
+++ Metic/Cell/PhotoRankingTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  PhotoRankingTableViewCell.m
+//  WeShare
+//
+//  Created by ligang6 on 14-9-23.
+//  Copyright (c) 2014年 dishcool. All rights reserved.
+//
+
+#import "PhotoRankingTableViewCell.h"
+
+@implementation PhotoRankingTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
