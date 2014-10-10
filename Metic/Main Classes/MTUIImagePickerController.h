2cc7c1fbc48aedc0907b1abf6a5f300c75c1e728
JJ <185597569@qq.com>
2014-10-10 15:43:15 +0800

    补充

---
+++ Metic/Main Classes/MTUIImagePickerController.h
@@ -0,0 +1,13 @@
+//
+//  MTUIImagePickerController.h
+//  WeShare
+//
+//  Created by ligang6 on 14-10-10.
+//  Copyright (c) 2014年 dishcool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTUIImagePickerController : UIImagePickerController
+
+@end
---
+++ Metic/Main Classes/MTUIImagePickerController.m
@@ -0,0 +1,58 @@
+//
+//  MTUIImagePickerController.m
+//  WeShare
+//
+//  Created by ligang6 on 14-10-10.
+//  Copyright (c) 2014年 dishcool. All rights reserved.
+//
+
+#import "MTUIImagePickerController.h"
+
+@interface MTUIImagePickerController ()
+
+@end
+
+@implementation MTUIImagePickerController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(NSUInteger)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskLandscapeRight;
+}
+
+-(BOOL)shouldAutorotate
+{
+    return YES;
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
