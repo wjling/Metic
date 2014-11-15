359d4f8083133f5e69f6c3430e21f561da9611d8
JJ <185597569@qq.com>
2014-11-15 10:09:49 +0800

    修复视频下载进度条导致的cpu占有过高

--- Metic/Cell/VideoWallTableViewCell.m
+++ Metic/Cell/VideoWallTableViewCell.m
@@ -412,9 +412,9 @@
         double delayInSeconds = self.progressOverlayView.stateChangeAnimationDuration;
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
         dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-            self.progressOverlayView.progress = 0.;
-            self.progressOverlayView.hidden = YES;
-            //self.videoPlayImg.hidden = NO;
+            [_progressOverlayView removeFromSuperview];
+            _progressOverlayView = nil;
+
         });
     }
     
