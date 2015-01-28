6a11db46c579ec6d883ddf9139457bf23c14c5e5
JJ <185597569@qq.com>
2015-01-28 18:10:50 +0800

    我的活动中没有活动时候，给个善意的提醒

--- Metic/Cell/MTTableView.m
+++ Metic/Cell/MTTableView.m
@@ -30,15 +30,34 @@
 #pragma mark 代理方法-UITableView
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    if (self.eventsSource) {
+    if (self.eventsSource && self.eventsSource.count != 0) {
         return self.eventsSource.count;
     }else
-        return 0;
+        return 1;
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    if (!self.eventsSource || self.eventsSource.count == 0) {
+        UITableViewCell* cell = [[UITableViewCell alloc]init];
+        UILabel* label = [[UILabel alloc]initWithFrame:CGRectMake(0,0,300,70)];
+        cell.userInteractionEnabled = NO;
+        cell.backgroundColor = [UIColor clearColor];
+        
+        
+        label.text = @"还没有活动哦，快去发起吧";
+        label.numberOfLines = 1;
+        label.backgroundColor = [UIColor clearColor];
+        label.font = [UIFont fontWithName:@"Helvetica-Bold" size:15];
+        label.textColor = [UIColor colorWithWhite:147.0/255.0 alpha:1.0f];
+        label.textAlignment = NSTextAlignmentCenter;
+        [cell addSubview:label];
+        
+        
+        return cell;
+    }
+
     static NSString *CellIdentifier = @"customcell";
     BOOL nibsRegistered = NO;
     if (!nibsRegistered) {
