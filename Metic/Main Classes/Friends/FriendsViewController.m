d5d59931f3c155e6e42103478c033dd9b4bb2924
Jason <185597569@qq.com>
2015-10-06 21:49:53 +0800

    删了好友之后，在活动参与者里面点击用户信息没有添加好友，而且还可以添加备注名(缓存问题)

--- Metic/Main Classes/Friends/FriendsViewController.m
+++ Metic/Main Classes/Friends/FriendsViewController.m
@@ -520,6 +520,7 @@
                         NSInteger fid_copy = [[friend_copy objectForKey:@"id"]integerValue];
                         if ([fid integerValue] == fid_copy) {
                             [[MTUser sharedInstance].friendList removeObjectAtIndex:i];
+                            [[MTUser sharedInstance].friendsIdSet removeObject:fid];
                             [[MTUser sharedInstance].alias_dic removeObjectForKey:[NSString stringWithFormat:@"%@",fid]];
                             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
                                 [[MTUser sharedInstance] aliasDicDidChanged];
