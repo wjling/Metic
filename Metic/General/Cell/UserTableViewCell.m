c1b0752cc683a987340701ab9c1c88bba7b893dd
dishcool <185597569@qq.com>
2016-05-22 16:35:44 +0800

    适配扫一扫

--- Metic/General/Cell/UserTableViewCell.m
+++ Metic/General/Cell/UserTableViewCell.m
@@ -7,6 +7,8 @@
 //
 
 #import "UserTableViewCell.h"
+#import "MTOperation.h"
+#import "PhotoGetter.h"
 
 @implementation UserTableViewCell
 
@@ -20,6 +22,18 @@
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+- (void)applyData:(NSDictionary *)data {
+    //显示备注名
+    NSString *alias = [MTOperation getAliasWithUserId:data[@"id"] userName:data[@"name"]];
+    
+    self.name.text = alias;
+    self.signature.text = ([[data valueForKey:@"sign"] isEqual:[NSNull null]])?@"":[data valueForKey:@"sign"];
+    self.location.text = ([[data valueForKey:@"location"] isEqual:[NSNull null]])?@"":[data valueForKey:@"location"];
+    self.genderImg.image = ([[data valueForKey:@"gender"] intValue] == 1)? [UIImage imageNamed:@"gender_male"]:[UIImage imageNamed:@"gender_female"];
+    PhotoGetter* avatarGetter = [[PhotoGetter alloc]initWithData:self.avatar authorId:[data valueForKey:@"id"]];
+    [avatarGetter getAvatar];
 }
 
 @end
