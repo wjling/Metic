1340db2e1e769c1858110cdef9338db2e5f44720
dishcool <185597569@qq.com>
2016-05-30 16:28:17 +0800

    个人信息页不能添加自己

--- Metic/Main Classes/Friends/FriendInfoViewController.m
+++ Metic/Main Classes/Friends/FriendInfoViewController.m
@@ -89,6 +89,9 @@
     {
         [self.navigationItem setTitle:@"好友信息"];
         self.navigationItem.rightBarButtonItem = nil;
+    } else if ([[MTUser sharedInstance].userid isEqualToNumber:fid]){
+        [self.navigationItem setTitle:@"个人信息"];
+        self.navigationItem.rightBarButtonItem = nil;
     }
     else
     {
